f0abfa94b57f9a9802c11331bd386f46bb170ffa
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-01-11 18:38:46 +0100

    There

--- play-with-text-files/src/org/vitu/files/SomeMoreTextFilesAwesomeness.java
+++ play-with-text-files/src/org/vitu/files/SomeMoreTextFilesAwesomeness.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 
-public class SomeMoreTextFiesAwesomeness {
+public class SomeMoreTextFilesAwesomeness {
 
 	public static void main(String[] args) {
 		
