6e7de02ad5bf81b4e58956d928c2855c5795fb48
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-01-13 12:42:10 +0100

    Second part of Reader done

--- play-with-text-files/src/org/vitu/files/PlayWithBufferedReader.java
+++ play-with-text-files/src/org/vitu/files/PlayWithBufferedReader.java
@@ -1,60 +1,32 @@
 package org.vitu.files;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.stream.Collectors;
 
 public class PlayWithBufferedReader {
 
 	public static void main(String[] args) {
 		
-		// PATTERN DE BASE POUR LIRE LE CONTENU D'UN FICHIER AVEC UN READER
+		// PATTERN AVEC UN BUFFEREDREADER POUR LIRE LE CONTENU D'UN FICHIER		
 		
-		// D'abord nous créons l'endroit où stocker les caractères lus dans quelque chose :
-		StringBuilder sb = new StringBuilder();
+		// Cette métode est beaucoup plus concise
 		
-		try (Reader reader = new FileReader("files/sonnet.txt");) {
-			// FileReader peut jeter deux exceptions : IOException & FileNotFoundException
-			// Or FileNotFoundException étends IOException
-		// } catch (FileNotFoundException e) {
-		// 	System.out.println("J'attrape la FNFE");
-		// 	e.printStackTrace();
-			// Donc il est inutile d'avoir les deux
-			
-			// Vu que le fichier est très petit, nous pouvons donner un petit buffer à notre tableau de caractères
-			char[] chars = new char[128];
-
-			// La méthode read(chars) nous retourne un entier du nombre de caractères qu'elle à lu
-			int number = reader.read(chars);
-			// Retourne 16, mais si nous passons [1_000_000], n'en retourne que 555
-			// Donc ce n'est pas parce que le tableau de caractères est grand qu'il est nécessairement rempli
-			// System.out.println("J'ai lu " + number + " caractères. ");
-			// Pour savoir quand le fichier à été entièrement lu, la méthode read() retourne '-1'
-			// Il faut donc le passer dans une boucle :
-			System.out.println(number + " lus.");
-			
-			while (number != -1) {
-				// Maintenant il faut append les caractères au StringBuilder
-				// Sans oublier d'ajouter les deux derniers paramètres pour qu'il sache de où à où les caractères doivent être lus
-				sb.append(chars, 0, number);
-				number = reader.read(chars);
-				
-				System.out.println(number + " lus.");
-				// Retourne :
-				// 128 lus.
-				// 128 lus.
-				// 128 lus.
-				// 128 lus.
-				// 96 lus.
-				// -1 lus.
-			}
+		String sonnet = null;
+		
+		try (Reader reader = new FileReader("files/sonnet.txt");
+			BufferedReader br = new BufferedReader(reader);) {
+			// Ici nous décorons notre Reader avec un BufferedReader
+			// L'avantage c'est que nous avons deux méthodes supplémentaires avec cette décoration : readLine() et lines() (qui retourne un Stream<> de Strings)
+			// Nous pouvons ainsi le concaténer dans un seul string à l'aide du Collector joining() avec le '\n' pour les séparer
+			sonnet = br.lines().collect(Collectors.joining("\n"));
 		} catch (IOException e) {
-			// System.out.println(e.getMessage());
 			System.out.println("J'attrape l'IOE");
 			e.printStackTrace();
-		} 
+		}
 		System.out.println("Contenu du fichier :");
-		System.out.println(sb.toString());
+		System.out.println(sonnet);
 	}
-
 }
